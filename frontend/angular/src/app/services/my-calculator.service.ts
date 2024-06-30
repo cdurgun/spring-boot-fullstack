028ad357abd353040b39175cca8c998a117ba29b
cdurgun <cdurgun@gmail.com>
2024-06-30 12:27:35 +0300

    Angular create project again

---
+++ frontend/angular/src/app/services/my-calculator.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyCalculatorService {
+
+  constructor() { }
+
+  sum(value1:number, value2:number):number  {
+    return +value1 + +value2;
+  }
+
+  sub(value1:number, value2:number):number  {
+    return value1 - value2;
+  }
+
+  multiply(value1:number, value2:number):number  {
+    return value1 * value2;
+  }
+
+  divide(value1:number, value2:number):number  {
+    return value1 / value2;
+  }
+
+}
